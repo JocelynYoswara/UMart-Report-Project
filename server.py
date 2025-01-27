e8363134ab31434c7e77b65c71608b021316c086
JocelynYoswara <jocelyn.yoswara@epos.com.sg>
2025-01-27 14:52:16 +0800

    file upload handle for server

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, send_file, render_template
+from flask_cors import CORS 
 import pandas as pd
 import os
 import re
 
 app = Flask(__name__)
+CORS(app) 
 
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
